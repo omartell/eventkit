1608c3422a7c5335731ec7811dd78f21838bc360
Oliver Martell <oliver.martell@gmail.com>
2015-05-24 14:03:16 +0100

    Extract event loop to separate gem

--- lib/eventkit.rb
+++ lib/eventkit.rb
@@ -1,7 +1,2 @@
-require 'eventkit/version'
 require 'eventkit/event_loop'
 require 'eventkit/promise'
-
-module Eventkit
-  # Your code goes here...
-end
--- lib/eventkit/version.rb
+++
@@ -1,3 +0,0 @@
-module Eventkit
-  VERSION = '0.0.4'
-end
